b577dd6a8d4b2beeca697e3edfe5ca5d2819ce58
Eduardo O.S <edumapri@gmail.com>
2023-07-12 21:33:18 -0300

    Exemplo003

--- 001-Inicial/src/App.java
+++ 001-Inicial/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Teste 2");
+        System.out.println("Teste");
     }
 }
---
+++ 002-JavaAnatomia/src/Exemplo003.java
@@ -0,0 +1,18 @@
+public class Exemplo003 {
+
+    public static void main(String[] args) {
+
+        String primeiroNome = "Eduardo";
+        String segundoNome = "Oliveira Silva";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+
+}
