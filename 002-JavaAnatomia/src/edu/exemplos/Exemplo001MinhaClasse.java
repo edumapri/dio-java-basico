3ebcf0072c327060a2e4e9c5437c8d37b5e14d80
Eduardo O.S <edumapri@gmail.com>
2023-07-13 14:10:46 -0300

    Ajuste dos arquivos em pacotes

---
+++ 002-JavaAnatomia/src/edu/exemplos/Exemplo001MinhaClasse.java
@@ -0,0 +1,9 @@
+package edu.exemplos;
+
+public class Exemplo001MinhaClasse {
+    public static void main(String[] args) {
+
+        System.out.print("002-JavaAnatomia");
+
+    }
+}
---
+++ 002-JavaAnatomia/src/edu/exemplos/Exemplo002ClasseReturn.java
@@ -0,0 +1,20 @@
+package edu.exemplos;
+
+public class Exemplo002ClasseReturn {
+
+    public static void main(String[] args) {
+
+        String primeiroNome = "Eduardo";
+        String segundoNome = "Oliveira Silva";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return "Resultado do método >> " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+
+}
