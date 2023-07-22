30ffac2318058b5831fbdfce886f4e554e4feb8f
Eduardo O.S <edumapri@gmail.com>
2023-07-22 15:59:44 -0300

    Operadores

--- 002-JavaAnatomia/src/edu/operadores-003/Exemplo004.java
+++ 002-JavaAnatomia/src/edu/operadores-003/Exemplo004.java
@@ -4,7 +4,7 @@ public class Exemplo004 {
         int numero2 = 2;
 
         if (numero1 > numero2)
-            System.out.printl("Numero 1 maior que número 2");
+            System.out.println("Numero 1 maior que número 2");
 
     }
 }
