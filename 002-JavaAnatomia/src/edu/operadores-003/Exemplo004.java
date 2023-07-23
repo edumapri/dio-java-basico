cbd26924f0274518b95b3ec22e6afd27d4b77040
Eduardo O.S <edumapri@gmail.com>
2023-07-23 15:29:22 -0300

    exemplo004

--- 002-JavaAnatomia/src/edu/operadores-003/Exemplo004.java
+++ 002-JavaAnatomia/src/edu/operadores-003/Exemplo004.java
@@ -6,5 +6,16 @@ public class Exemplo004 {
         if (numero1 > numero2)
             System.out.println("Numero 1 maior que número 2");
 
+        if (numero1 < numero2)
+            System.out.println("Numero 1 menor que numero 2");
+
+        if (numero1 >= numero2)
+            System.out.println("Numero 1 maior ou igual que numero 2");
+
+        if (numero1 <= numero2)
+            System.out.println("Numero 1 menor ou igual que numero 2");
+
+        if (numero1 != numero2)
+            System.out.println("Numero 1 é diferente de numero 2");
     }
 }
