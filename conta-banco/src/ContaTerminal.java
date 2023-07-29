c2375a02d2cd4d4bf902d70adc994ae7f5cd3cde
Eduardo O.S <edumapri@gmail.com>
2023-07-29 19:12:01 -0300

    update

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -3,29 +3,31 @@ import java.util.Scanner;
 
 public class ContaTerminal {
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
-        Scanner entradaUsuario = new Scanner(System.in).useLocale(Locale.US);
+                Scanner entradaUsuario = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.print("Digite o numero de sua conta: ");
-        int conta = entradaUsuario.nextInt();
+                System.out.print("Digite o numero de sua conta: ");
+                int conta = entradaUsuario.nextInt();
 
-        System.out.print("Digite o número de sua agência: ");
-        String agencia = entradaUsuario.next();
+                System.out.print("Digite o número de sua agência: ");
+                String agencia = entradaUsuario.next();
 
-        System.out.print("Digite o seu primeiro nome: ");
-        String nome = entradaUsuario.next();
+                System.out.print("Digite o seu primeiro nome: ");
+                String nome = entradaUsuario.next();
 
-        System.out.print("Digite o seu sobrenome: ");
-        String sobrnome = entradaUsuario.next();
+                System.out.print("Digite o seu sobrenome: ");
+                String sobrnome = entradaUsuario.next();
 
-        String cliente = nome + " " + sobrnome;
-          
-        System.out.print("Informe o valor de seu depósito inicial: ");
-        double saldo = entradaUsuario.nextDouble();
+                String cliente = nome + " " + sobrnome;
 
-        System.out.println("Olá " + cliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia
-                + ", conta " + conta + " e o seu saldo " + saldo + " já está disponível para saque.");
+                System.out.print("Informe o valor de seu depósito inicial: ");
+                double saldo = entradaUsuario.nextDouble();
 
-        entradaUsuario.close();
+                System.out.println("Olá " + cliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                                + agencia
+                                + ", conta " + conta + " e o seu saldo " + saldo + " já está disponível para saque.");
+
+                entradaUsuario.close();
+        }
 }
